3f841129282933093beb480c062ba83f02fe7c0b
muhammadsarimwaseem <sarimwaseem84@gmail.com>
2023-08-14 16:56:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var list = document.getElementById("list");
+
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("Initializing hacking......");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 2000);
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("Hacking Sarim's device......");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 4000);
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("Hacking all social media accounts......");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 6000);
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("h̸̨̖͇̤̹̙̖̼̳̦̜̲͉̭̜̠̔̀̿̋͜͝ả̵̢̢̡̨̧̨̛̳̰̻̹̦̖͈͕̺͙͕̝͚̗͈̝͉̘͈̰̫̹̰̯̭͆́̓͊̀̈́͆̿̇͑̀̌͌̽͒̌͑̈́̓̉́̄̒̎̊̀̃̐̍̒̇̈́́̑̽̽̈́̈́̈́̑̈̃͘͘͘͝͝͝͝ç̴̨̢̢͇̤̪̟̺̙̖̟̤̘̟̹̩̩̫͙̩̩̪̞̩͔̘̝͚̹̥̳͈͉̦͌̊̐̈͂͒͋̄͘͜͜͝ḱ̶̡̡̘̜̯̺͓̖̮̪̩̲͙̥̯͓̥͉̗̮͕̤̱̠̟̰͕̣̼̫͈̮̭̯̩̫̜̥̥̘̱̃̃̇̋͐̇̆͒́̿͌̿̈́̓̆̉̓͊͐͑̏̓̀̀́̋̽̂̿̏̾̉͂̀̄͋͘͘͜͜͝");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 8000);
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("Fetching all the data......");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 10000);
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("h̵̢̢̢̡̨̧̡̧̞͇͔͖̜̳͍̣̺͉͎̝̮͍̬͎̩̘͕͇͙͈͖͈̟̟̱̻̼͓̜̣̱̮̲̮͈͈͎͇̹̖̫͎̭̲̾͜ͅą̵̢̛̛̬͓̬̹͎̫͔̮͍̭̲̫͓̝̘̩̺͎͎̩̪̜̜̼̲̀̀̀̾̓̀͂̒̏̅͌̈̓̐́͂̓̃͊̌̂̓̂̅̂̌̔̌͊̾̚̚͜͝c̵̡̡̨̢̢̡̢̡̬̟̣̘̗͈̜̹͇̺͈̭̳͍͖̖͇̳̟͖̘̜͍̥̺͓̳͖̺̹̦͖̠͚̯͕̫̝̹̥̥̬̬̫̪̹͖̪̘̳̣͉̤̬̗͈̳͇̻̳̟̲͔̮̻̥̳̱̻͙̩̲̹̙̃̎̒͌̄̓̉͗̊̃̾̆̑̏͗̾̚̚͜͜͠͝͝ḱ̵̛̛̛̖̂̏̆̒̉̈́̍̂̀̋̽̆̌͒̽̇̓̀̈́̈́͋̃̅̀̓̍̑̾̀́̾̃̃͌̔̓͊̉̄̉̌̀̂̀̽͘̕͘͜͠͝e̶̢̨̡̢̧̧̛͓̜͚̭͕͎̫̜̫̞͍̻̥͖̱̰̺̣͖͓͔̘̫͍͙͈̩̞̬͎͔͙̤̳̱̳͖̥͍̘̜͓͚̦̠͖̭͔̱̻͓̼̪̭̪̮̣͇͚̼͔̥͚̠̫̥̼̱̽͊̏͋͆̂̿͆͋̔̄̓͗͊̉̔̇̀̏̒̆́̄̍̌̅̒̿̉͑̑͋̈́̉͌̓̎̉͐̅͒̏̈́͐̈́̃̊̈̌́̎̂̿̀̊͂̑̀́͆͛͐̚̚͘̚͘͜͜͜͜͜͜͠͠͝͠͝͠͝͝͝ͅͅͅr̵̡̠̱͕͉̮͙͍̭͇̝͚̜̝̞̱̭̼͓̰̤̞̳̲̦̹̃̇̎̉́̅̊̆̎̽͆̃͋̔̂̒̊͛̐̃̑̿̈́̒̈̈́̒́̿́̅̕͝͠͝͝ͅ");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 11000);
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("s̴̪̟̳̭̘̜̼̞̥̣̙̲̬̟͚̱̤̗̥̜̽̀̀̎̀̅͜ͅä̴̢̡̧̢̛̜͕͕̫̲͈̩̜͇̹̠̱͇̼̺͕͇̗͔̥̮̰̀̉́̆͛̈́̓͜r̸̢̰̮̰̝̪̺̞̞̀̑̆̓̍̂̈́͆̋̅̈͗̈́͊͑̉̌̌̅͗̊̚͘͜͝í̵̢̡̛͓̰̖̞͉̙̒͋̓̈́̏̔͛̽̈́̾̍̈́̾͑͋̏̕͝͝͝ṁ̸̪͖̝̯̺̬͉͓̗̗̹̬̹̝͖͇̰͋̄̄̓̅̚͜ͅͅͅ");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 11000);
+setTimeout(() => {
+  var li = document.createElement("li");
+  var text = document.createTextNode("!!!!!!!!!!!!Hack done!!!!!!!!!!!");
+  li.appendChild(text);
+  list.appendChild(li);
+}, 13000);
